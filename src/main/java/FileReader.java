f9e8aabd00f55e1a0b98b007374fceada473cc9e
tdudek1 <thomas.dudek@gmail.com>
2017-08-08 00:08:08 -0500

    Add logging and file enumaration

---
+++ src/main/java/FileReader.java
@@ -0,0 +1,18 @@
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
+
+import java.io.File;
+import java.util.Collection;
+
+/**
+ * Created by Thomas Dudek on 8/7/2017.
+ */
+public class FileReader {
+
+ public Collection<File> GetFiles(String path){
+
+     return FileUtils.listFiles(new File(path),new String[] { "mp3"},true);
+
+ }
+}
--- src/main/java/mp3fix.java
+++ src/main/java/mp3fix.java
@@ -1,9 +1,26 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Collection;
+
 /**
  * Created by tdudek on 8/7/17.
  */
 public class mp3fix {
+
+    final static Logger logger = LoggerFactory.getLogger(mp3fix.class);
+
     public static void main(String [] args)
     {
-        System.out.println("Test;");
+
+        logger.info("Staring...");
+
+        Collection<File> files = (new FileReader()).GetFiles("C:\\Users\\Thomas Dudek\\Desktop\\test\\Polski Pop");
+
+        for(File file : files)
+        {
+            logger.info(String.format("Filename %s",file.getName()));
+        }
     }
 }
